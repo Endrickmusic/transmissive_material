5c9fb08bb524ae716601f03ca5591ce2852f1f0c
Christian <mail@christianhohenbild.de>
2024-01-17 12:58:21 +0100

    initial commit

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,23 +1,35 @@
-import { OrbitControls, RoundedBox } from "@react-three/drei"
+import { OrbitControls, MeshTransmissionMaterial } from "@react-three/drei"
 import { useLoader } from "@react-three/fiber"
 import * as THREE from "three"
+import { RGBELoader } from 'three-stdlib'
 
 
 export default function Experience(){
 
-  const normalM = useLoader(THREE.TextureLoader, "./Textures/waternormals.jpeg"); 
+
+  const normalM = useLoader(THREE.TextureLoader, "./Textures/waternormals.jpeg")
+  const texture = useLoader(RGBELoader, 'Environments//envmap.hdr')
 
   return (
     <>
       <OrbitControls />       
-        <RoundedBox
-          radius={0.01}
-          >
-          <meshStandardMaterial 
-            metalness={1}
-            roughness={0.12}
-            normalMap={ normalM }
-          />
-       </RoundedBox>
+      <mesh>
+        <boxGeometry />
+        <MeshTransmissionMaterial
+        // ref={refMaterial}
+        // onBeforeCompile={onBeforeCompile}
+          attach="material"
+          background={texture}
+          reflectivity={0.5}
+          roughness={0.35}
+          transmission={0.8}
+          thickness={0.5}
+          color={'#ffffff'}
+          ior={1.3}
+          distortionScale = {1}
+          distortion = {0.9}
+          temporalDistortion = {0.1}
+        />
+      </mesh>
     </>
   )}
